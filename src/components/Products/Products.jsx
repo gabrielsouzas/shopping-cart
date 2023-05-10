6752b09cbcbcfd6a04590643326c3ab0fb6771c0
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-10 16:50:24 -0300

    Componente Products

---
+++ src/components/Products/Products.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import './Products.css';
+
+function Products() {
+  return (
+    <section className="products container"></section>
+  );
+}
+
+export default Products;
