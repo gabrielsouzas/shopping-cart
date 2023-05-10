b43767ee10ca999c0e194abf7e94053bc861f4b1
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-10 17:47:41 -0300

    Update ProductCard

--- src/components/ProductCard/ProductCard.jsx
+++ src/components/ProductCard/ProductCard.jsx
@@ -1,9 +1,26 @@
 import React from 'react';
 
+import './ProductCard.css';
+import { BsFillCartPlusFill } from 'react-icons/bs';
+
 function ProductCard() {
   return (
     <section className="product-card">
-      product card
+      
+      <img 
+        src="https://http2.mlstatic.com/D_910401-MLA46545868460_062021-W.jpg" 
+        alt="product" 
+        className="card__image" />
+
+      <div className="card__infos">
+        <h2 className="card__price">R$ 200.00</h2>
+        <h2 className="card__title">Manual do Dev</h2>
+      </div>
+
+      <button type="button" className="button__add-cart">
+        <BsFillCartPlusFill />
+      </button>
+
     </section>
   );
 }
