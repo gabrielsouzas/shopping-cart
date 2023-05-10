4b24b894a28da9f12c4c66f33f4b27f1196fc228
Gabriel Souza <104937852+gabrielsouzas@users.noreply.github.com>
2023-05-10 17:11:33 -0300

    Componente ProductCard

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -1,10 +1,25 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import './Products.css';
+import fetchProducts from '../../api/fetchProducts';
+import ProductCard from '../ProductCard/ProductCard';
 
 function Products() {
+
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetchProducts('iphone').then((response) => {
+      setProducts(response);
+      console.log(products);
+    });
+
+  }, []);
+
   return (
-    <section className="products container"></section>
+    <section className="products container">
+      <ProductCard />
+    </section>
   );
 }
 
